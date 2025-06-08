2209dcb4a0815ce08401a78b05568d00f09023a7
kk2023 <2366419768@qq.com>
2025-06-09 07:01:15 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
 import { ConfigProvider } from 'antd';
-import "antd/dist/antd.compact.css"
+//import "antd/dist/antd.compact.css";
 import zhCN from 'antd/locale/zh_CN';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App.tsx'
