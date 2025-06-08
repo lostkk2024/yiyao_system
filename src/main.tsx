2ffc8dcde39edf0d94c2e5f505fb4306609a1f3c
kk2023 <2366419768@qq.com>
2025-06-09 06:32:24 +0800

    first_commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { ConfigProvider } from 'antd';
+import "antd/dist/antd.compact.css"
+import zhCN from 'antd/locale/zh_CN';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App.tsx'
+
+
+createRoot(document.getElementById('root')!).render(
+  <Router>
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
+  </Router>
+
+)
